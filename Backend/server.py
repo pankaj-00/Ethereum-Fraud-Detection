a855df3296e4c8cd0d8e3e2e7ada634936a71027
Yash-Ambekar <yashambekar03@gmail.com>
2022-10-08 18:37:24 +0530

    Corrected somethings and added requirements file

--- Backend/server.py
+++ Backend/server.py
@@ -2,33 +2,29 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from joblib import load
 import numpy as np
-import pickle
-import json
 
-X_Address = load('X_Address.joblib')
-clf = load("Ethereum_Fraud_Detection.joblib")
+
 
 # Initializing flask app
 app = Flask(__name__)
 CORS(app) 
 
 
-# with open('model_pickle', 'rb') as f:
-#     clf =pickle.load(f)
-        
-# with open('X_Address_pickle', 'rb') as f:
-#     X_Address =pickle.load(f)
+X_Address = load('X_Address.joblib')
+clf = load("Ethereum_Fraud_Detection.joblib")
   
 @app.route('/predict', methods=["POST"])
 @cross_origin(origin='*',headers=['Content- Type','Authorization'])
 
+#Sample Address = 0x00009277775ac7d0d59eaad8fee3d10ac6c805e8
+
 def prediction_func():
         data = request.json
-        temp = X_Address.where(X_Address['Address'] == "0x00009277775ac7d0d59eaad8fee3d10ac6c805e8")
+        temp = X_Address.where(X_Address['Address'] == data[1:-1])
         X_info = temp.dropna().iloc[:,0:44]
-        print(X_info)
-        prediction = clf.predict(X_info)
-        response = jsonify({"result": "The Prediction is: "+ prediction[0]})
+        # print(X_info) #get information related to that data point 
+        prediction = clf.predict_proba(X_info)
+        response = jsonify({"result": "The Prediction is: "+ str(prediction[0][0])})
         return response         
     
       
